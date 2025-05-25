0f27669582a6a3ff90a2e2337e5622c864dc5571
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-05-25 13:01:36 +0530

    export statements for all services/database functions

---
+++ src/services/database/index.ts
@@ -0,0 +1,5 @@
+// Export all the modules in the database folder for use in the app
+
+export * from './connection';
+export * from './operations/patientOperations';
+export * from './sync/syncManager'; 
